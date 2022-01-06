add157b1df5b1bdd65c0d6018b7beb39ff450c0d
Ayoub-Ioudiden <93585615+Ayoub-Ioudiden@users.noreply.github.com>
2022-01-06 12:15:56 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const makePizza = function () {
+    console.log("make pizza dough");
+    console.log("make pizza sauce");
+    console.log("put the sauce on the dough");
+    console.log("put the toppings on the pizza dough");
+    console.log("put the pizza in the over for 10min");
+    console.log("take it out the oven and cut it");
+};
+
+makePizza();
+makePizza();
+
+
+const makeSushi = function () {
+    console.log("Make the sticky rice");
+    console.log("Prepare the fish");
+    console.log("Combine the rice and fish");
+    console.log("Season it and serve");
+
+};
+
+makeSushi();
+makeSushi();
+makeSushi();
